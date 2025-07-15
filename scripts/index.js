f8eb6d0a256ef2f2df6822ae63b3b22857423a21
cheeky-2 <jjuing0@gmail.com>
2025-07-15 18:17:16 +0900

    자바스크립트 작업중

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ const wrap = new Swiper('#wrap',{
     mousewheel:true,
 })
 const project1 = new Swiper('#project_swiper',{
-    scrollbar:{el:'#project_swiper~.swiper-scrollbar'},
+    /* scrollbar:{el:'#project_swiper~.swiper-scrollbar'}, */
     navigation:{
         nextEl:'#project_swiper~.swiper-button-next',
         prevEl:'#project_swiper~.swiper-button-prev',
@@ -12,17 +12,32 @@ const project1 = new Swiper('#project_swiper',{
 /* SNS 프로젝트 */
 const snsSwiper = new Swiper('#sns_swiper',{
     slidesPerView:3,
-    spaceBetween:10,
+    spaceBetween:30,
     autoplay:{delay:0,},
     speed:20000,
     loop:true,
 })
+const bnrSwiper = new Swiper('#bnr_swiper',{
+    slidesPerView:3,
+    spaceBetween:30,
+    autoplay:{delay:0,},
+    speed:10000,
+    loop:true,
+})
+const detailSwiper = new Swiper('#detail_swiper',{
+    effect:'fade',
+    direction:'vertical',
+    slidesPerView:1,
+    autoplay:{delay:2000,},
+    speed:1000,
+    loop:true,
+})
 // SNS 프로젝트 클릭 시 팝업 실행(클릭한 이미지가 팝업 이미지로 교체)
-const snsProject = document.querySelectorAll('#sns_swiper .swiper-slide');
+const designProject = document.querySelectorAll('.container .contents .swiper .swiper-wrapper .swiper-slide');
 const popup = document.querySelector('.popup_bg');
-for(let sns of snsProject){
-    sns.addEventListener('click',()=>{
-        popup.children[0].children[0].src = sns.children[0].src;
+for(let design of designProject){
+    design.addEventListener('click',()=>{
+        popup.children[0].children[0].src = design.children[0].src;
         popup.style.display = 'inline-block';
         //팝업 실행 시 전체 수직 Swiper 스크롤 기능 막기
         wrap.mousewheel.disable();//스크롤 풀기 enable()
@@ -41,5 +56,26 @@ nav.forEach((obj, idx)=>{
     obj.addEventListener('click',(e)=>{
         e.preventDefault(); //a의 href기본기능막기
         wrap.slideTo(idx, 1000)
+    })
+})
+const menuBtn = document.querySelector('nav button');
+for(let i of nav){
+    menuBtn.addEventListener('click',()=>{
+        /* if(i.style.opacity = 0){
+            i.style.opacity = 1;
+            i.style = 'pointer-events:none';
+            i.style.transition = 'all 1s';
+        } */
+    })
+}
+const descriptions = document.querySelectorAll('.skill li em');
+const descriptionsIcon = document.querySelectorAll('.skill li span');
+descriptionsIcon.forEach((obj, idx)=>{
+    obj.addEventListener('click',()=>{
+        const visible = descriptions[idx].style.display === 'block';
+        descriptions.forEach(description=>description.style.display='none');
+        if(!visible){
+            descriptions[idx].style.display='block';
+        }
     })
 })
